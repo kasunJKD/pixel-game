37e030889c407bff361b31426ecfcc3171f5eefa
Kasun Jayakody <64832263+kasunJKD@users.noreply.github.com>
2024-05-16 22:38:49 +0530

    player textures added

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
 const rl = @import("raylib");
+const Texture2D = rl.Texture2D;
+const Vec2 = rl.Vector2;
 
 pub fn main() anyerror!void {
     // Initialization
@@ -8,27 +10,27 @@ pub fn main() anyerror!void {
     const screenWidth = 800;
     const screenHeight = 450;
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    rl.initWindow(screenWidth, screenHeight, "pixel template");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
+    const player = rl.loadTexture("assets/game_player.png");
+    defer player.unload();
+
+    const position = Vec2.init(
+        @as(f32, @floatFromInt(@divTrunc((screenWidth - player.width), 2))),
+        @as(f32, @floatFromInt(@divTrunc((screenHeight - player.height), 2))),
+    );
+    //--------------------------------------------------------------------------------------
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        rl.clearBackground(rl.Color.white);
+        rl.drawTextureV(player, position, rl.Color.white);
 
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
-        //----------------------------------------------------------------------------------
+        rl.clearBackground(rl.Color.white);
     }
 }
